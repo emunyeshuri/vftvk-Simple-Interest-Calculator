a917a836a69aae2bcfdcab5516f86f9b7fab66c9
Macbook Pro <macbookpro@MacBook-Pro-Macbook.local>
2022-05-25 15:47:43 +0200

    yes

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+let minval=1;
+let maxval=20;
+let default_val=10.25;
+var principal=document.getElementById("principal").value;
+var rate= document.getElementById("rate");
+var years=document.getElementById("years");
+var interest=principal*years*rate/100;
+var year=new Date().getFullYear()+parseInt(years);
+function updaterate(){
+    var rateval=document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
 function compute()
 {
     p = document.getElementById("principal").value;
